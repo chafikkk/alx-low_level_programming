d31296a7d1fc70307fb9095f9023c2ee19eed935
chafikkk <chafik.rd@gmail.com>
2023-06-18 15:03:28 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Printfs  all single digit numbers of base 10 start 0
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58 ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+
