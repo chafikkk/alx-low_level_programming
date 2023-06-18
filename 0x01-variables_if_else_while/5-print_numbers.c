b5d5af5342838e6cae45b24bb3a4ebc5cb6ec91f
chafikkk <chafik.rd@gmail.com>
2023-06-18 14:48:18 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Printfs  all single digit numbers of base 10 starting from 0
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int i = 0;
+for (i = 0; i < 10 ; i++)
+{
+printf("%d", i);
+}
+return (0);
+}
+
