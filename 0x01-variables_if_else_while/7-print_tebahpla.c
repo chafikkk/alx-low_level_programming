9f62e653cbc5c9c178d9b2f0847b438613c665fc
chafikkk <chafik.rd@gmail.com>
2023-06-18 15:09:03 -0700

    commit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 
 /**
-*main - Printfs  all single digit numbers of base 10 start 0
+*main - Printfs prints the lowercase alphabet in reverse
 *
 * Return: Always 0.
 */
 int main(void)
 {
 int i;
-for (i = 48; i < 58 ; i++)
+for (i = 122; i > 96 ; i--)
 {
 putchar(i);
 }
