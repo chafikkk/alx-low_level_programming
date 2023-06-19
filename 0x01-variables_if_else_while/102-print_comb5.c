6e885eab6c74b0f23d9cb20d5bdddec9e07a819c
chafikkk <chafik.rd@gmail.com>
2023-06-18 18:03:28 -0700

    commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Printfs all possible combinations of two two-digit numbers.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+int i, j, k, l;
+for (i = 0; i < 100 ; i++)
+{
+for (j = 0; j < 100 ; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
