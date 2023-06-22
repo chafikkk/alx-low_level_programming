c63aeae3212870331b3311593e4b242c9b356b85
chafikkk <chafik.rd@gmail.com>
2023-06-22 04:53:12 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,15 +7,15 @@
 void jack_bauer(void);
 {
 int i, j;
-for(i = 0; i <= 23; i++)
+for (i = 0; i <= 23; i++)
 {
 for (j = 0; j < 60; j++)
 {
-_putchar(((i/10)+48));
-_putchar(((i%10)+48));
+_putchar(((i / 10) + 48));
+_putchar(((i % 10) + 48));
 _putchar(':');
-_putchar(((j/10)+48));
-_putchar(((j%10)+48));
+_putchar(((j / 10) + 48));
+_putchar(((j % 10) + 48));
 _putchar('\n');
 }
 }
