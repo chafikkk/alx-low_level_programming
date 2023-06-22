daaa3f6c34045a7e583056a08c1068860af93956
chafikkk <chafik.rd@gmail.com>
2023-06-22 05:53:31 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,12 +10,20 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
+if (i == 2 && j == 5)
+{
+_putchar(49);
+_putchar(48);
+}
+else
+{
 res = i * j;
 if (res >= 12)
 _putchar(((res / 10) + 48));
 else
 _putchar(' ');
 _putchar(((res % 10) + 48));
+}
 if (j != 9)
 _putchar(',');
 _putchar(' ');
