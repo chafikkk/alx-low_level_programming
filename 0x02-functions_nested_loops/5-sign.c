d22c56341cd9c58d7fa93bcda857dbf96e0811ed
chafikkk <chafik.rd@gmail.com>
2023-06-22 01:51:04 -0700

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,14 @@ if (n > 0)
 _putchar(43);
 return (1);
 }
+if (n == 0)
+{
+_putchar (48);
+return (0);
+}
 if (n < 0)
 {
 _putchar(45);
 return (-1);
-}
-if (n == 0)
-{
-_putchar(48);
-return (0);
 }
 }
