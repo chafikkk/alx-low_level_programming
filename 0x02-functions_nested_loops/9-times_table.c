c5803e59f55b24787bdc52efdbc0701ed934e488
chafikkk <chafik.rd@gmail.com>
2023-06-25 10:02:26 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,6 +10,11 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
+int i, j, res, rb;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
 res = i * j;
 rb  = i * (j - 1);
 if (j != 0)
@@ -19,29 +24,24 @@ if (rb <= 9 && res < 10)
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar(' ');
 }
 if (rb <= 9 && res >= 10)
 {
 _putchar(',');
-_putchar(' ');
 _putchar(' ');
 }
 if (rb >= 10 && res >= 10 && res < 100)
 {
 _putchar(',');
 _putchar(' ');
-_putchar(' ');
-}
-if (rb >= 90 && res >= 100)
-{
-_putchar(',');
-_putchar(' ');
 }
 }
-_putchar((res % 10) + 48);
+if (res >= 10)
 _putchar((res / 10) + 48);
+_putchar((res % 10) + 48);
 }
 _putchar('\n');
+}
+}
 }
 }
